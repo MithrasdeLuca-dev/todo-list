6193b8e29df1abcad7dceb4348d239e5e21d62be
MithrasdeLuca-dev <mdlucata@gmail.com>
2023-02-07 00:28:50 -0400

    Create fomr tasks

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,15 @@
 import styles from './Header.module.css'
 
-import todoLogo from '../assets/todolist.svg'
+import todoLogo from '../assets/todologo.svg'
+import todoName from '../assets/todoname.svg'
 
-export function Header(){
+export function Header() {
   return (
     <header className={styles.header}>
-      <img src= {todoLogo} alt='Logotipo do Todo'/>
+      <div className= {styles.logo}>
+        <img src={todoLogo} alt='Logotipo do Todo' />
+        <img src={todoName} alt='Nome do Todo' />
+      </div>
     </header>
   );
 }
